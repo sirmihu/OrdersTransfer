74110d22f6bd3606d5cf66b839877db7a21312b8
sirmihu <michal.penczek@gmail.com>
2023-06-23 20:13:13 +0200

    Added transfer faire orders to baselinker logic

---
+++ TransferFaireOrdersToBaselinker/TransferFaireOrdersToBaselinker/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Xml.Linq;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using FaireApiNs = FaireApi;
+using FaireApi.Utils;
+using BaselinkerApi.Utils;
+using BaselinkerApiNs = BaselinkerApi;
+using Microsoft.Extensions.Configuration;
+using FaireApi.Settings;
+using BaselinkerApi.Settings;
+
+[assembly: FunctionsStartup(typeof(TransferFaireOrdersToBaselinker.Startup))]
+namespace TransferFaireOrdersToBaselinker
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            //builder.Services.AddOptions<FaireApiSettings>()
+            //.Configure<IConfiguration>((settings, configuration) =>
+            //{
+            //    configuration.GetSection("FaireApiSettings").Bind(settings);
+            //});
+
+            //builder.Services.AddOptions<BaselinkerApiSettings>()
+            //.Configure<IConfiguration>((settings, configuration) =>
+            //{
+            //    configuration.GetSection("BaselinkerApiSettings").Bind(settings);
+            //});
+
+            builder.Services.AddTransient<IFaireApiHttpClient, FaireApiHttpClient>();
+            builder.Services.AddTransient<FaireApiNs.IFaireApi, FaireApiNs.FaireApi>();
+
+            builder.Services.AddTransient<IBaselinkerApiHttpClient, BaselinkerApiHttpClient>();
+            builder.Services.AddTransient<BaselinkerApiNs.IBaselinkerApi, BaselinkerApiNs.BaselinkerApi>();
+        }
+    }
+}
+
--- TransferFaireOrdersToBaselinker/TransferFaireOrdersToBaselinker/TransferFaireOrdersToBaselinker.cs
+++ TransferFaireOrdersToBaselinker/TransferFaireOrdersToBaselinker/TransferFaireOrdersToBaselinker.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BaselinkerApi;
+using BaselinkerApi.Models;
+using BaselinkerApi.Requests;
+using FaireApi;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -7,9 +14,63 @@ namespace TransferFaireOrdersToBaselinker
 {
     public class TransferFaireOrdersToBaselinker
     {
-        [FunctionName("TransferFaireOrdersToBaselinker")]
-        public void Run([TimerTrigger("*/10 * * * *")]TimerInfo myTimer, ILogger log)
+        private readonly IFaireApi _faireApi;
+        private readonly IBaselinkerApi _baselinkerApi;
+
+        public TransferFaireOrdersToBaselinker(IFaireApi faireApi, IBaselinkerApi baselinkerApi)
         {
+            _faireApi = faireApi;
+            _baselinkerApi = baselinkerApi;
+        }
+
+        [FunctionName("TransferFaireOrdersToBaselinker")]
+        public async Task Run([TimerTrigger("*/10 * * * *")]TimerInfo myTimer, ILogger log)
+        {
+            var faireOrders = await _faireApi.GetAllOrders();
+            var faireOrdersInBaselinker = (await _baselinkerApi.GetOrders(
+                new GetOrdersRequest
+                {
+                    FilterOrderSourceId = 1024
+                })).Orders;
+
+            foreach (var faireOrder in faireOrders)
+            {
+                if (faireOrdersInBaselinker.Any(p => p.ExtraField1 == faireOrder.Id))
+                    continue;
+
+                await _baselinkerApi.AddOrder(new AddOrderRequest
+                {
+                    OrderStatusId = 8069,
+                    CustomSourceId = 1024,
+                    ExtraField1 = faireOrder.Id,
+                    DateAdd = (int)((DateTimeOffset)DateTime.Parse(faireOrder.CreatedAt)).ToUnixTimeSeconds(),
+                    DeliveryFullname = faireOrder.Address.Name,
+                    DeliveryCompany = faireOrder.Address.CompanyName,
+                    DeliveryAddress = $"{faireOrder.Address.Address1},{faireOrder.Address.Address2}",
+                    DeliveryPostcode = faireOrder.Address.PostalCode,
+                    DeliveryCity = faireOrder.Address.City,
+                    DeliveryState = faireOrder.Address.State,
+                    DeliveryCountryCode = faireOrder.Address.CountryCode,
+                    InvoiceFullname = faireOrder.Address.CompanyName,
+                    InvoiceCompany = faireOrder.Address.CompanyName,
+                    InvoiceAddress = $"{faireOrder.Address.Address1},{faireOrder.Address.Address2}",
+                    InvoicePostcode = faireOrder.Address.PostalCode,
+                    InvoiceCity = faireOrder.Address.City,
+                    InvoiceState = faireOrder.Address.State,
+                    InvoiceCountryCode = faireOrder.Address.CountryCode,
+                    Phone = faireOrder.Address.PhoneNumber,
+                    Products = faireOrder.Items.Select(item =>
+                    new Product
+                    {
+                        Quantity = item.Quantity,
+                        Sku = item.Sku,
+                        PriceBrutto = item.PriceCents,
+                        Name = item.ProductName,
+
+                    }).ToList()
+                });
+            }
+
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
         }
     }
